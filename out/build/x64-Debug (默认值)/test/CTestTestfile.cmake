bc347aab3a208521fd66ba88dcca540e2b5ad5ce
shuaigegegegeg <1539698483@qq.com>
2024-12-22 23:14:19 +0800

    d1

---
+++ out/build/x64-Debug (默认值)/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: E:/vs s/cs/jitu/nrenderer/code/test
+# Build directory: E:/vs s/cs/jitu/nrenderer/code/out/build/x64-Debug (默认值)/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(NR_GTest "NR_GTest")
+set_tests_properties(NR_GTest PROPERTIES  _BACKTRACE_TRIPLES "E:/vs s/cs/jitu/nrenderer/code/test/CMakeLists.txt;13;add_test;E:/vs s/cs/jitu/nrenderer/code/test/CMakeLists.txt;0;")
